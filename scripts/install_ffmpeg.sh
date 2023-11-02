d258edd72604d402f2a3434ec4bbb38e834563bc
Li Wan <lwan3@student.unimelb.edu.au>
2023-11-02 13:35:58 +1100

    Make sure ffmpeg can be install on arm64

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -16,10 +16,12 @@ if [[ "$TARGETPLATFORM" == "linux/arm64" ]]; then
   mv /temp/ffmpeg-static/ffmpeg /usr/bin
   mv /temp/ffmpeg-static/ffprobe /usr/bin
   rm -r /temp/ffmpeg-static
+  alternatives --set java /usr/lib/jvm/java-17-openjdk-17.0.6.0.9-0.3.ea.el8.aarch64/bin/java
 elif [[ "$TARGETPLATFORM" == "linux/amd64" ]]; then
   # For AMD64 platform
   dnf config-manager --add-repo=https://negativo17.org/repos/epel-multimedia.repo
   dnf install -y ffmpeg
+  alternatives --set java /usr/lib/jvm/java-17-openjdk-17.0.6.0.9-0.3.ea.el8.x86_64/bin/java
 else
   # Handle unsupported platforms here, if needed
   echo "WARNING: Unsupported target platform: $TARGETPLATFORM"
