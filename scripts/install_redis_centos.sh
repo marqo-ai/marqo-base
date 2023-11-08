e09dee0d1ca0aa97ff9d94290478560a8687e75d
Li Wan <lwan3@student.unimelb.edu.au>
2023-11-08 17:08:23 +1100

    Remove default value for targetplatform and fix Redis install

--- scripts/install_redis_centos.sh
+++ scripts/install_redis_centos.sh
@@ -2,10 +2,6 @@
 # This script is meant to be run at buildtime.
 
 # Add the EPEL repository which contains Redis for CentOS
-dnf install -y epel-release
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-# Update packages
-dnf update -y
-
-# Install Redis
-dnf install -y redis
+dnf module install redis:remi-7.2 -y
