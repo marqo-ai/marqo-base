0bc608b82b8c6b36fa7834ffb8dcb364efe9502d
Li Wan <li@marqo.ai>
2024-11-06 13:42:28 +1100

    Add punkt tab download

--- scripts/install_punkt_tokenizers.sh
+++ scripts/install_punkt_tokenizers.sh
@@ -5,3 +5,7 @@ mkdir -p /root/nltk_data/tokenizers
 curl https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt.zip -o /root/nltk_data/tokenizers/punkt.zip
 unzip /root/nltk_data/tokenizers/punkt.zip -d /root/nltk_data/tokenizers/
 rm /root/nltk_data/tokenizers/punkt.zip
+
+curl https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt_tab.zip -o /root/nltk_data/tokenizers/punkt_tab.zip
+unzip /root/nltk_data/tokenizers/punkt_tab.zip -d /root/nltk_data/tokenizers/
+rm /root/nltk_data/tokenizers/punkt_tab.zip
