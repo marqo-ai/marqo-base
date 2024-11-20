232e1aba3cd9a7505861d46ca41493c70e94c3f0
Li Wan <li@marqo.ai>
2024-11-20 16:37:59 +1100

    Remove nltk punkt

--- scripts/install_punkt_tokenizers.sh
+++ scripts/install_punkt_tokenizers.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# This script is meant to be run at buildtime.
+# This script is meant to be run at build time.
 set -euo pipefail
 set -x
 
 mkdir -p /root/nltk_data/tokenizers
-curl https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt.zip -o /root/nltk_data/tokenizers/punkt.zip
-unzip /root/nltk_data/tokenizers/punkt.zip -d /root/nltk_data/tokenizers/
-rm /root/nltk_data/tokenizers/punkt.zip
-
 curl https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt_tab.zip -o /root/nltk_data/tokenizers/punkt_tab.zip
 unzip /root/nltk_data/tokenizers/punkt_tab.zip -d /root/nltk_data/tokenizers/
 rm /root/nltk_data/tokenizers/punkt_tab.zip
